b730efd79fa623adfffc358f53581bc69178b545
Piotr Regulski <bulu901@gmail.com>
2025-05-21 19:49:42 +0200

    added polyline

--- src/components/map/MapComponent.tsx
+++ src/components/map/MapComponent.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup,Polyline } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 
@@ -22,31 +22,38 @@ const markerIcon: L.Icon = new L.Icon({
 
 const MapComponent: React.FC<UserPosition> = () => {
   const [userPosition, setUserPosition] = useState<UserPosition | null>(null);
+  const [track, setTrack] = useState<UserPosition[]>([]); // 🔹 Tablica współrzędnych trasy
 
+  
   useEffect(() => {
     if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(
+      const watchId = navigator.geolocation.watchPosition(
         (position: GeolocationPosition) => {
-          setUserPosition({
-            lat: position.coords.latitude,
-            lon: position.coords.longitude,
-          });
+          const newPosition = { lat: position.coords.latitude, lon: position.coords.longitude };
+
+          setUserPosition(newPosition);
+          setTrack((prevTrack) => [...prevTrack, newPosition]); // 🔹 Dodanie nowej pozycji do trasy
         },
         (error: GeolocationPositionError) => console.error("❌ Błąd GPS:", error.message),
-        { enableHighAccuracy: true, timeout: 10000, maximumAge: 0 }
+        { enableHighAccuracy: true, timeout: 5000, maximumAge: 0 }
       );
+
+      return () => navigator.geolocation.clearWatch(watchId);
     }
   }, []);
-
   return (
-    <div>
+     <div>
       {userPosition ? (
-    
-        <MapContainer center={[userPosition.lat, userPosition.lon]} zoom={13} className="h-[25rem] w-screen  rounded-lg shadow-lg border-2 border-lime-950 mx-4">
+        <MapContainer center={[userPosition.lat, userPosition.lon]} zoom={18} className="h-[25rem] w-screen rounded-lg shadow-lg border-2 border-lime-950 mx-4">
           <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution="&copy; OpenStreetMap contributors" />
+
+          {/* 🔹 Marker użytkownika */}
           <Marker position={[userPosition.lat, userPosition.lon]} icon={markerIcon}>
             <Popup>📍 Twoja aktualna lokalizacja</Popup>
           </Marker>
+
+          {/* 🔹 Rysowanie trasy */}
+          {track.length > 1 && <Polyline positions={track.map((pos) => [pos.lat, pos.lon])} color="blue" />}
         </MapContainer>
       ) : (
         <p className="text-center">⏳ Pobieranie Twojej lokalizacji...</p>
