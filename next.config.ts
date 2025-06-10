fbfb5dc15ca3887aaa779551efb720b39d5ae8e4
Piotr Regulski <bulu901@gmail.com>
2025-06-10 20:19:57 +0200

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,18 @@
 import withPWAInit from "@ducanh2912/next-pwa";
 
+
 const withPWA = withPWAInit({
   dest: "public",
-  cacheOnFrontEndNav:true,
-  aggressiveFrontEndNavCaching:true,
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
-  disable:true,
+  disable: false, // włącz PWA
   workboxOptions: {
-    disableDevLogs:true,
-}});
+    disableDevLogs: true,
+  
+  },
+});
 
 export default withPWA({
-
-  // Your Next.js config
+  // Tutaj możesz dodać inne opcje Next.js
 });
